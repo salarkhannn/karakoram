7e6cde2e934790506939583362da529bb92e5575
Salar Khan <salarkhanlala@gmail.com>
2025-02-04 23:42:16 +0500

    styled home page'

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import bgImg from "../assets/webCover.png";
 
 export default function HomePage() {
     return (
         <div className="home-page h-screen flex flex-col">
+            <div 
+                className="bg-img absolute inset-0 bg-cover bg-center bg-no-repeat -z-10"
+                style={{ backgroundImage: `url(${bgImg})` }}
+            ></div>
             <Navbar />
             <div className="title-container flex flex-col justify-center items-center flex-grow">
                 <p className="title">conquer</p>
