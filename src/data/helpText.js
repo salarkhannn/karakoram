3ae28c2d1a33eeb37539d677b355dfabd0bb88e3
Salar Khan <salarkhanlala@gmail.com>
2025-02-04 19:28:01 +0500

    created help page and did some styling

---
+++ src/data/helpText.js
@@ -0,0 +1,80 @@
+const orderFAQ = [
+    {
+        title: "How do I place an order?",
+        description: "To place an order, simply add the items you want to purchase to your"
+    },
+    {
+        title: "What payment methods do you accept?",
+        description: "We accept Visa, MasterCard, PayPal, and Apple Pay for your convenience."
+    },
+    {
+        title: "Can I modify or cancel my order after placing it?",
+        description: "Unfortunately, once your order is confirmed, we are unable to make changes. Please double-check your order before submitting."
+    }
+];
+
+const shippingFAQ = [
+    {
+        title: "How long does shipping take?",
+        description: "We process orders within 2-3 business days. Shipping usually takes between 5-7 business days depending on your location."
+    },
+    {
+        title: "Do you offer international shipping?",
+        description: "Yes! We offer worldwide shipping. Additional fees may apply for international orders."
+    },
+    {
+        title: "How can I track my order?",
+        description: "Once your order ships, you will receive an email with tracking information. You can use this to track it on carrier's website"
+    }
+];
+
+const returnFAQ = [
+    {
+        title: "What is your return policy?",
+        description: "We accept returns within 30 days of delivery."
+    },
+    {
+        title: "How do I initiate a return?",
+        description: "Please contact our customer service team to initiate the return process."
+    },
+    {
+        title: "How long does it take to process returns?",
+        description: "Returns are processed within 5-7 business days. Refunds will be issued to your original payment method."
+    }
+];
+
+const productFAQ = [
+    {
+        title: "How do I find my correct size?",
+        description: "Please refer to our Size Guide on each product page. We recommend measuring yourself and comparing your measurements to the size chart."
+    },
+    {
+        title: "What materials are used in your products?",
+        description: "All of our products are made from high-quality, sustainable materials. You’ll find detailed material information on each product page."
+    },
+    {
+        title: "Do you restock sold-out items?",
+        description: "We restock popular items regularly. Sign up for Back in Stock Alerts to be notified when your desired item is available again."
+    }
+];
+
+const allFAQ = [
+    {
+        faqTitle: "Ordering & Payments",
+        faq: orderFAQ
+    },
+    {
+        faqTitle: "Shipping & Delivery",
+        faq: shippingFAQ
+    },
+    {
+        faqTitle: "Returns & Refunds",
+        faq: returnFAQ
+    },
+    {
+        faqTitle: "Product Information",
+        faq: productFAQ
+    }
+];
+
+export default allFAQ;
--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,10 +1,27 @@
 import Navbar from "../components/Navbar";
+import allFAQ from "../data/helpText";
+
 
 export default function Help(){
     return (
         <div className="help">
             <Navbar />
-            <h1>Help</h1>
+            <p className="help-title2">Frequently Asked Questions (FAQ)</p>
+            <ul className="help-list">
+                {allFAQ.map((faqCategory) => (
+                    <li key={faqCategory.faqTitle}>
+                        <p className="help-title3">{faqCategory.faqTitle}</p>
+                        <ul>
+                            {faqCategory.faq.map((item, index) => (
+                                <li key={index}>
+                                    <p className="faq-question">{item.title}</p>
+                                    <p className="faq-answer">{item.description}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
