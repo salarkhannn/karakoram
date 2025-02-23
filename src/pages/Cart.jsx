58349ba8dbc2eb0660cadf754f92494ef7767e18
Salar Khan <salarkhanlala@gmail.com>
2025-02-23 18:15:17 +0500

    design changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { CartContext } from "../components/CartContext";
+import { Link } from "react-router-dom";
 
 export default function Cart(){
     const { cart, removeFromCart } = useContext(CartContext);
@@ -14,21 +15,29 @@ export default function Cart(){
         <>
             <Navbar />
             <div className="cart-container font-['Neue']">
-                <h1 className="ml-70">Your Cart</h1>
                 {cart.length === 0 ? (
-                    <p className="">Your cart is empty</p>
-                ) : (
-                    <>
-                        <div className="flex flex-row ml-70 w-[70vw]">
-                            <div className="w-[15vw]"></div>
-                            <div className="flex flex-row justify-between w-[70vw]">
-                                <p className="ml-10 pb-5">ITEM</p>
-                                <p className="">Total</p>
+                    <div className="no-results min-h-screen flex flex-col">
+                        <div className="flex-grow flex items-center justify-center">
+                            <div className="text-center mt-[-100px]">
+                                <p className="text-[5rem] font-medium text-black font-[Neue]">No items in cart yet</p>
+                                <Link to={'/shop'} className="text-gray-500 text-[1.5rem] font-[Neue] hover:underline">Add items</Link>
                             </div>
                         </div>
-                        <ul className="cart-list flex flex-col ml-70">
+                        <Footer />
+                    </div>
+                ) : (
+                    <>
+                        <h1 className="ml-[15vw]">Your Cart</h1>
+                        <div className="flex flex-row ml-[12.5vw] w-[70vw]">
+                            <div className="w-[15vw]"></div>
+                            <div className="flex flex-row justify-between w-[70vw]">
+                                <p className="ml-[140px] pb-5">ITEM</p>
+                                <p className="ml-[140px] pl-5">Total</p>
+                            </div>
+                        </div>
+                        <ul className="cart-list flex flex-col ml-[15vw]">
                             {cart.map((item) => (
-                                <li key={item.name} className="cart-item w-100">
+                                <li key={[item.name, item.size]} className="cart-item w-[70vw]">
                                     <hr className="w-[70vw] opacity-60"/>
                                     <div className="flex flex-row justify-between w-[70vw] pt-10 pb-10">
                                         <div className="flex flex-row">
@@ -56,17 +65,17 @@ export default function Cart(){
                                 </li>
                             ))}
                         </ul>
-                        <hr className="ml-70 mr-70 mb-5"/>
+                        <hr className="ml-[15vw] mr-[15vw] mb-5"/>
                         <div className="flex flex-col pb-20">
-                            <div className="flex flex-row justify-between w-[70vw] ml-70">
+                            <div className="flex flex-row justify-between w-[70vw] ml-[15vw]">
                                 <p className="">Total</p>
                                 <p className="">${total}</p>
                             </div>
-                            <div className="flex flex-row justify-between w-[70vw] ml-70">
+                            <div className="flex flex-row justify-between w-[70vw] ml-[15vw]">
                                 <p className="">Shipping Estimate</p>
                                 <p className="">Calculated at checkout</p>
                             </div>
-                            <div className="flex flex-row justify-between w-[70vw] ml-70 mt-2">
+                            <div className="flex flex-row justify-between w-[70vw] ml-[15vw] mt-2">
                                 <p className="font-bold">Order Total</p>
                                 <p className="font-bold">${total}</p>
                             </div>
